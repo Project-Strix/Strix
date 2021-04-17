4f803d655ab0171185dae79fbab77b37acffee28
Chenglong Wang <clwang@phy.ecnu.edu.cn>
2021-04-17 16:53:25 +0800

    Change default dir to home dir. Update version to 0.0.5.

--- medlp/configures/config.py
+++ medlp/configures/config.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 
 DEFAULT_MEDLP_CONFIG = {
     "config_fname": 'medlp_configures.cfg',
-    "OUTPUT_DIR": "/homes/clwang/Data/medlp_exp",
-    "EXTERNAL_DATASET_DIR": "/homes/clwang/Data/medlp_datasets",
+    "OUTPUT_DIR": str(Path.home()/'Data'/'medlp_exp'),
+    "EXTERNAL_DATASET_DIR": str(Path.home()/'Data'/'medlp_datasets'),
 }
