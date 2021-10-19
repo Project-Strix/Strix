c68f90e2bd2475ea1fedd4c6038ec527b089f1c6
Chenglong Wang <clwang@phy.ecnu.edu.cn>
2021-10-19 14:09:40 +0800

    Change default project dir to Path.home()/'MeDLP'.

--- medlp/configures/config.py
+++ medlp/configures/config.py
@@ -5,9 +5,9 @@ from configparser import ConfigParser
 DEFAULT_MEDLP_CONFIG = {
     "MODE": 'dev',  # release
     "CONFIG_FNAME": 'medlp_configures.cfg',
-    "OUTPUT_DIR": str(Path.home()/'Data'/'medlp_exp'),
-    "EXTERNAL_DATASET_DIR": str(Path.home()/'Code'/'medlp_datasets'),
-    "EXTERNAL_NETWORK_DIR": str(Path.home()/'Code'/'medlp_networks'),
+    "OUTPUT_DIR": str(Path.home()/'MeDLP'/'medlp_exp'),
+    "EXTERNAL_DATASET_DIR": str(Path.home()/'MeDLP'/'medlp_datasets'),
+    "EXTERNAL_NETWORK_DIR": str(Path.home()/'MeDLP'/'medlp_networks'),
 }
 
 
