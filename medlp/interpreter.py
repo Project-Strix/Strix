ff4a94b4e4fac162e5a9df082ded79273df54616
Chenglong Wang <clwang@phy.ecnu.edu.cn>
2021-11-15 11:46:18 +0800

    Update monai_ex ver. pep8 fixes.

--- medlp/interpreter.py
+++ medlp/interpreter.py
@@ -13,37 +13,54 @@ from ignite.utils import setup_logger
 
 from medlp.models import get_test_engine
 from medlp.data_io.dataio import get_dataloader
-from medlp.utilities.click_ex import (
-    NumericChoice as Choice,
-    parse_input_str
-)
+from medlp.utilities.click_ex import NumericChoice as Choice, parse_input_str
 from medlp.utilities.click_callbacks import get_trained_models
-from medlp.utilities.handlers import GradCamHandler
+from monai_ex.handlers import GradCamHandler
 
 
-@click.command('gradcam-from-cfg')
-@click.option("--config", type=click.Path(exists=True), default="",
-              help='Specify config file')
-@click.option("--test-files", type=str, default="",
-              help="External files (json/yaml) for testing")
-@click.option('--target-layer', '-L', type=click.UNPROCESSED,
-              prompt=True, callback=partial(parse_input_str, dtype=str),
-              help='Target network layer for cam visualization')
-@click.option('--target-class', type=int, prompt=True, default=1,
-              help='GradCAM target class, should be 0 for single chn output')
-@click.option('--method', type=Choice(['gradcam', 'layercam']), default='gradcam',
-              help='Choose visualize method')
-@click.option("--out-dir", type=str, default=None,
-              help="Optional output dir to save results")
-@click.option("--fusion", is_flag=True,
-              help='Normal fusion of multiple CAM maps')
-@click.option("--hierarchical", is_flag=True,
-              help="Hierarchically fuse muliple CAM maps")
-@click.option("--debug", is_flag=True,
-              help='Debug mode. In debug mode, n_sample=1')
-@click.option("--n-sample", type=int, default=-1,
-              help="Number of samples to be processed. Defulat=-1")
-@click.option('--gpus', prompt="Choose GPUs[eg: 0]", type=str)
+@click.command("gradcam-from-cfg")
+@click.option(
+    "--config", type=click.Path(exists=True), default="", help="Specify config file"
+)
+@click.option(
+    "--test-files", type=str, default="", help="External files (json/yaml) for testing"
+)
+@click.option(
+    "--target-layer",
+    "-L",
+    type=click.UNPROCESSED,
+    prompt=True,
+    callback=partial(parse_input_str, dtype=str),
+    help="Target network layer for cam visualization",
+)
+@click.option(
+    "--target-class",
+    type=int,
+    prompt=True,
+    default=1,
+    help="GradCAM target class, should be 0 for single chn output",
+)
+@click.option(
+    "--method",
+    type=Choice(["gradcam", "layercam"]),
+    default="gradcam",
+    help="Choose visualize method",
+)
+@click.option(
+    "--out-dir", type=str, default=None, help="Optional output dir to save results"
+)
+@click.option("--fusion", is_flag=True, help="Normal fusion of multiple CAM maps")
+@click.option(
+    "--hierarchical", is_flag=True, help="Hierarchically fuse muliple CAM maps"
+)
+@click.option("--debug", is_flag=True, help="Debug mode. In debug mode, n_sample=1")
+@click.option(
+    "--n-sample",
+    type=int,
+    default=-1,
+    help="Number of samples to be processed. Defulat=-1",
+)
+@click.option("--gpus", prompt="Choose GPUs[eg: 0]", type=str)
 def gradcam(**args):
     if "CUDA_VISIBLE_DEVICES" in os.environ:
         Print("CUDA_VISIBLE_DEVICES specified, ignoring --gpu flag")
@@ -54,25 +71,25 @@ def gradcam(**args):
     exp_dir = Path(configures.get("experiment_path", os.path.dirname(args["config"])))
 
     if configures.get("n_fold", 0) > 1:
-        raise NotImplementedError('Donot support cross-valid experiment')
+        raise NotImplementedError("Donot support cross-valid experiment")
 
     if os.path.isfile(args["test_files"]):
         test_fpath = args["test_files"]
         test_files = get_items_from_file(args["test_files"], format="auto")
     else:
-        test_fpaths = list(exp_dir.glob('valid_files*'))
+        test_fpaths = list(exp_dir.glob("valid_files*"))
         if len(test_fpaths) > 0:
             test_fpath = test_fpaths[0]
             test_files = get_items_from_file(test_fpath, format="auto")
         else:
             raise ValueError(f"Test file does not exists in {exp_dir}!")
 
-    if args['debug']:
+    if args["debug"]:
         test_files = test_files[:1]
-    elif args['n_sample'] > 0:
-        test_files = test_files[:args['n_sample']]
+    elif args["n_sample"] > 0:
+        test_files = test_files[: args["n_sample"]]
 
-    phase = 'test_wo_label'
+    phase = "test_wo_label"
     configures["preload"] = 0.0
     configures["phase"] = phase
     configures["experiment_path"] = exp_dir
@@ -91,10 +108,10 @@ def gradcam(**args):
     logging_level = logging.DEBUG if args["debug"] else logging.INFO
     engine.logger = setup_logger(f"{args['method']}-interpreter", level=logging_level)
 
-    if args['fusion'] and args['hierarchical']:
-        raise ValueError('fusion and hierarchical cannot be both True')
+    if args["fusion"] and args["hierarchical"]:
+        raise ValueError("fusion and hierarchical cannot be both True")
 
-    if args['hierarchical'] and args['method'] == 'gradcam':
+    if args["hierarchical"] and args["method"] == "gradcam":
         engine.logger.warn("Gradcam do not support hierarchical fusion")
 
     engine.add_event_handler(
@@ -105,14 +122,15 @@ def gradcam(**args):
             args["target_class"],
             engine.data_loader,
             engine.prepare_batch,
-            method=args['method'],
-            fusion=args['fusion'],
-            hierarchical=args['hierarchical'],
+            method=args["method"],
+            fusion=args["fusion"],
+            hierarchical=args["hierarchical"],
             save_dir=configures["out_dir"],
             device=torch.device("cuda")
-            if args['gpus'] != "-1" else torch.device("cpu"),
-            logger_name=engine.logger.name
-        )
+            if args["gpus"] != "-1"
+            else torch.device("cpu"),
+            logger_name=engine.logger.name,
+        ),
     )
 
     engine.run()
