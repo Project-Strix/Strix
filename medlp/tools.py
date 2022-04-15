67964ea89f30b77cd31c1954b8833208d4202ab1
Chenglong Wang <ryuu.j.ching@gmail.com>
2022-04-15 13:32:26 +0800

    Add summarize-data functionality.  Enable to summary 'Spacing', 'Statistics' and 'Percentiles' of data.

--- medlp/tools.py
+++ medlp/tools.py
@@ -1,22 +1,26 @@
-import os
 import csv
+import os
 from pathlib import Path
+
 import click
-from utils_cw import Print, get_items_from_file
 import matplotlib as mpl
+from utils_cw import Print, get_items_from_file
 
 mpl.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
+from medlp.data_io import DATASET_MAPPING
+from medlp.utilities.click_ex import NumericChoice as Choice
+from medlp.utilities.click_callbacks import data_select
+from medlp.utilities.enum import FRAMEWORKS, Phases
+from medlp.configures import config as cfg
+from monai_ex.data import DatasetSummaryEx
+
 
 @click.command("merge-roc")
-@click.option(
-    "--exp-dir", type=click.Path(exists=True), help="Dir contains test folders"
-)
-@click.option(
-    "--dirname-as-legend", type=bool, default=True, help="Use dirname as legend label"
-)
+@click.option("--exp-dir", type=click.Path(exists=True), help="Dir contains test folders")
+@click.option("--dirname-as-legend", type=bool, default=True, help="Use dirname as legend label")
 def merge_roc_curves(exp_dir, dirname_as_legend):
     """Utility for mergeing multiple ROC curves.
 
@@ -59,9 +63,7 @@ def merge_roc_curves(exp_dir, dirname_as_legend):
             auc_value = get_items_from_file(cls_result_file).get("AUC", -1)
             auc_values.append(auc_value)
         else:
-            auc_value = click.prompt(
-                f"Input ROC value for {roc_file.parent.name}", type=float
-            )
+            auc_value = click.prompt(f"Input ROC value for {roc_file.parent.name}", type=float)
             auc_values.append(auc_value)
 
     # Draw ROC
@@ -73,9 +75,7 @@ def merge_roc_curves(exp_dir, dirname_as_legend):
     for auc, roc, legend in sorted(zip(auc_values, roc_files, legend_names)):
         with open(roc) as f:
             reader = csv.DictReader(f)
-            fpr_tpr = np.array(
-                [[float(row["FPR"]), float(row["TPR"])] for row in reader]
-            )
+            fpr_tpr = np.array([[float(row["FPR"]), float(row["TPR"])] for row in reader])
         plt.plot(fpr_tpr[:, 0], fpr_tpr[:, 1], label=f"{legend} AUC={auc:.2f}")
     plt.plot([0, 1], [0, 1], "k--")
     plt.xlabel("False positive rate")
@@ -83,3 +83,67 @@ def merge_roc_curves(exp_dir, dirname_as_legend):
     plt.legend(loc="lower right", fontsize="x-large")
     plt.savefig(os.path.join(exp_dir, "merged_roc.png"))
     Print("ROC is saved to", os.path.join(exp_dir, "merged_roc.png"), color="g")
+
+
+options = ["Spacing", "Statistics", "Percentiles"]
+
+
+@click.command("summarize-data")
+@click.option("--tensor-dim", prompt=True, type=Choice(["2D", "3D"]), default="2D", help="2D or 3D")
+@click.option("--framework", prompt=True, type=Choice(FRAMEWORKS), default="segmentation", help="Choose framework")
+@click.option("--data-list", type=str, callback=data_select, default=None, help="Data file list")
+@click.option("--skip", "-s", prompt=True, prompt_required=False, type=Choice(options), default=None)
+def summarize_data(tensor_dim, framework, data_list, skip):
+    data_attr = DATASET_MAPPING[framework][tensor_dim][data_list]
+    dataset_fn, dataset_list = data_attr["FN"], data_attr["PATH"]
+    files_list = get_items_from_file(dataset_list, format="auto")
+
+    try:
+        train_dataset = dataset_fn(files_list, Phases.TRAIN, {"preload": 0})
+    except Exception as e:
+        Print(f"Creating dataset '{data_list}' failed! \nMsg: {repr(e)}", color="r")
+        return
+
+    analyzer = DatasetSummaryEx(
+        dataset=train_dataset,
+        image_key=cfg.get_key("image"),
+        label_key=cfg.get_key("label"),
+        num_workers=2,  #! os.cpu_count()
+        select_transforms=1,
+    )
+
+    if skip is None or "Spacing" not in skip:
+        Print("Begin analysis spacing...", color="g")
+        spacing_summary = analyzer.get_target_spacing(
+            spacing_key="pixdim",
+            anisotropic_threshold=3,
+            percentile=10,
+        )
+        print("=> Spacing:", spacing_summary)
+
+    if skip is None or "Statistics" not in skip:
+        Print("Begin analysis statistics...", color="g")
+        analyzer.calculate_statistics(foreground_threshold=0)
+        print(
+            f"=> Statistics:\n"
+            f"\tdata_max: {analyzer.data_max}\n"
+            f"\tdata_min: {analyzer.data_min}\n"
+            f"\tdata_mean: {analyzer.data_mean}\n"
+            f"\tdata_std: {analyzer.data_std}\n"
+        )
+
+    if skip is None or "Percentiles" not in skip:
+        Print("Begin analysis percentiles...", color="g")
+        analyzer.calculate_percentiles(
+            foreground_threshold=0,
+            sampling_flag=True,
+            interval=10,
+            min_percentile=0.5,
+            max_percentile=99.5,
+        )
+        print(
+            f"=> Percentiles:\n",
+            f"\tdata_min_percentile: {analyzer.data_min_percentile}"
+            f"\tdata_max_percentile: {analyzer.data_max_percentile}"
+            f"\tdata_median: {analyzer.data_median}"
+        )
