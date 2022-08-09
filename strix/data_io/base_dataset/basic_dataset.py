426ac82bd631333fa7bcb3c15c4175af90c2e13b
ChenglongWang <ryuu.j.ching@gmail.com>
2022-08-09 18:23:03 +0800

    37 profiling (#53)
    * Update setup.cfg to add nvtx as extras_require.
    Change python_requires >= 3.8.
    
    * feat(cli): Add `--profile` flag to turn on GPU profiling
    
    * feat(datasets): Add profiling support to StrixDataset.
    
    * chore(cli): Add `profiling.sh` script (not tested).
    
    * Rename profile script to `strix-profile`.
    Update setup.py
    
    * Minor fixed nsys output file name.
    
    * Update readme.
    
    Co-authored-by: Chenglong Wang <clwang@phy.ecnu.edu.cn>

--- strix/data_io/base_dataset/basic_dataset.py
+++ strix/data_io/base_dataset/basic_dataset.py
@@ -1,8 +1,10 @@
 from typing import Optional, Sequence, Union
 
 from monai_ex.data import Dataset
-from monai_ex.transforms import MapTransform, ComposeEx as Compose
-from monai_ex.utils import ensure_list
+from monai_ex.transforms import ComposeEx as Compose
+from monai_ex.transforms import MapTransform
+from monai_ex.utils import Range, ensure_list
+
 from strix.data_io.base_dataset.utils import get_input_data
 
 
@@ -24,6 +26,7 @@ class StrixDataset(object):
         dataset_kwargs: dict,
         additional_transforms: Optional[Sequence[MapTransform]] = None,
         check_data: bool = True,
+        profiling: bool = False,
         verbose: bool = False,
     ):
         self.filelist = filelist
@@ -31,34 +34,38 @@ class StrixDataset(object):
         self.dataset = dataset_type
         self.dataset_kwargs = dataset_kwargs
         if check_data:
-            self.input_data = get_input_data(
-                filelist, is_supervised, verbose, self.__class__.__name__
-            )
+            self.input_data = get_input_data(filelist, is_supervised, verbose, self.__class__.__name__)
         else:
             self.input_data = filelist
 
+        def _wrap_range(transforms):
+            if profiling:
+                return [Range(tsf.__class__.__name__)(tsf) for tsf in ensure_list(transforms)]
+            else:
+                return ensure_list(transforms)
+
         self.transforms = []
         if loader is not None:
-            self.transforms += ensure_list(loader)
+            self.transforms += _wrap_range(loader)
         if channeler is not None:
-            self.transforms += ensure_list(channeler)
+            self.transforms += _wrap_range(channeler)
         if orienter is not None:
-            self.transforms += ensure_list(orienter)
+            self.transforms += _wrap_range(orienter)
         if spacer is not None:
-            self.transforms += ensure_list(spacer)
+            self.transforms += _wrap_range(spacer)
         if rescaler is not None:
-            self.transforms += ensure_list(rescaler)
+            self.transforms += _wrap_range(rescaler)
         if resizer is not None:
-            self.transforms += ensure_list(resizer)
+            self.transforms += _wrap_range(resizer)
         if cropper is not None:
-            self.transforms += ensure_list(cropper)
+            self.transforms += _wrap_range(cropper)
         if additional_transforms is not None:
-            self.transforms += ensure_list(additional_transforms)
+            self.transforms += _wrap_range(additional_transforms)
 
         if caster is not None:
-            self.transforms += ensure_list(caster)
+            self.transforms += _wrap_range(caster)
         if to_tensor is not None:
-            self.transforms += ensure_list(to_tensor)
+            self.transforms += _wrap_range(to_tensor)
 
         if self.transforms:
             self.transforms = Compose(self.transforms)
@@ -66,6 +73,3 @@ class StrixDataset(object):
             self.transforms = None
 
         return self.dataset(self.input_data, transform=self.transforms, **self.dataset_kwargs)
-
-
-StrixClassificationDataset = StrixSegmentationDataset = StrixSelflearningDataset = StrixDataset
